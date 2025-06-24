0044ae96486995ce18de7f68ed76b8ba498dad87
unknown <kajugadaniels@gmail.com>
2025-06-24 13:39:41 -0700

    Add fetchSliderDetail to api.js

--- src/api.js
+++ src/api.js
@@ -1066,3 +1066,17 @@ export async function fetchSliders(params = {}) {
     throw err.response?.data || err;
   }
 }
+
+/**
+ * Fetch detailed information for a single slider by ID.
+ * @param {number} sliderId
+ * @returns {Promise<import('axios').AxiosResponse>}
+ */
+export async function fetchSliderDetail(sliderId) {
+  try {
+    const { data } = await apiClient.get(`/sliders/${sliderId}`);
+    return data;
+  } catch (err) {
+    throw err.response?.data || err;
+  }
+}
